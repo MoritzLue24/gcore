50725dbd51aca27820b656e14387049ac983a959
MoritzLue24 <moluecke@gmx.de>
2024-03-13 22:51:28 +0100

    export append_cfg

--- gcore/__init__.py
+++ gcore/__init__.py
@@ -1,5 +1,6 @@
 from . import image
+from . import ui
 from .animation import Animation
 from .entity import Entity
-from .config import set_cfg, get_cfg
+from .config import set_cfg, append_cfg, get_cfg
 
