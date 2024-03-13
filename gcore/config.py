ba73b300d484886400116e5f088ea026d4014560
MoritzLue24 <moluecke@gmx.de>
2024-03-13 22:50:13 +0100

    added append_cfg

--- gcore/config.py
+++ gcore/config.py
@@ -1,14 +1,14 @@
 
 __cfg_set = False
 __config = {
-    "scale": 5
+    "scale": 5,
+    "dialogue_width": 650,
+    "dialogue_height": 200
 }
 
 def set_cfg(config: dict):
-    """
-    only can be called once,
-    make sure to call before creating objects
-    """
+    """only can be called once,
+    make sure to call before creating objects"""
     global __cfg_set, __config
 
     if __cfg_set:
@@ -16,6 +16,11 @@ def set_cfg(config: dict):
 
     __cfg_set = True
     __config = config.copy()
+
+def append_cfg(config: dict):
+    """appends keys & values to the config"""
+    global __config
+    __config.update(config)
 
 def get_cfg(key: str):
     return __config[key]
