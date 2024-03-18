785e7c0e49c3d13bbd92139b3a0410407023e3ba
MoritzLue24 <moluecke@gmx.de>
2024-03-18 22:20:16 +0100

    item class, combined draw & update fn

--- gcore/__init__.py
+++ gcore/__init__.py
@@ -1,6 +1,17 @@
 from . import image
 from . import ui
+from .config import set_cfg, append_cfg, get_cfg
 from .animation import Animation
 from .entity import Entity
-from .config import set_cfg, append_cfg, get_cfg
+from .item import Item
+
+
+def update_all():
+    Entity.update()
+    Item.update()
+
+def draw_all(surface):
+    Entity.draw(surface)
+    Item.draw(surface)
+    ui.Dialogue.draw(surface)
 
---
+++ gcore/item.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class Item:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def update():
+        pass
+
+    @staticmethod
+    def draw(surface: pygame.Surface):
+        pass
